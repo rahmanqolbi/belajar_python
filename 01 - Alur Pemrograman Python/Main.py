5bb1bc13618c39d5fd17a1e9ccda9eac7870387b
rahmanqolbi <98144116+rahmanqolbi@users.noreply.github.com>
2022-03-31 16:45:44 +0700

    Add files via upload

---
+++ 01 - Alur Pemrograman Python/Main.py
@@ -0,0 +1,16 @@
+print("Alur Pemrograman Python")
+
+print("Yaaa")
+# ini adalah comment
+print("Seep")
+x=5 # ini adalah comment
+"""ini adalah comment
+tapi agak beda
+yang dibawah akan menampilkan 
+nilai x"""
+print(x)
+# kita bisa mengcompile python ke
+# yang namanya bytecode
+# cara mengcompile dengan mengetik di
+# terminal
+# python -m py_compile Main.py
