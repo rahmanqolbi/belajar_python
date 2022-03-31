c80b3e7a9c96256b7fd00edf0fb0bf29cb9743b5
rahmanqolbi <98144116+rahmanqolbi@users.noreply.github.com>
2022-03-31 16:45:06 +0700

    Add files via upload

---
+++ 03 - Tipe Data/Main.py
@@ -0,0 +1,8 @@
+data_integer = 1
+print("data :", data_integer, ", bertipe :", type(data_integer))
+data_float = 2.5
+print("data :", data_float, ", bertipe :", type(data_float))
+data_string = "Kata-kata"
+print("data :", data_string, ", bertipe :", type(data_string))
+data_boolean = True
+print("data :", data_boolean, ", bertipe :", type(data_boolean))
---
+++ 04 - Casting Tipe Data/Main.py
@@ -0,0 +1,12 @@
+# merubah dari satu tipe ke tipe lain
+
+data_integer = 9
+print("data :",data_integer,", tipe :",type(data_integer))
+
+data_float = float(data_integer)
+data_str = str(data_integer)
+data_bool = bool(data_integer)
+print("data :",data_float,", tipe :",type(data_float))
+print("data :",data_str,", tipe :",type(data_str))
+print("data :",data_bool,", tipe :",type(data_bool))
+
