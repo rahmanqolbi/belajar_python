88b9591a9a1d271d55440b98a8f6584808900bfe
rahmanqolbi <98144116+rahmanqolbi@users.noreply.github.com>
2022-03-31 16:43:26 +0700

    Add files via upload

---
+++ 05 - Input Data dari User/Main.py
@@ -0,0 +1,11 @@
+# data yang dimasukan pasti string
+data = input("Masukan Data = ")
+print("Data =",data, type(data))
+
+# jika ingin input integer
+data_int = int(input("Masukan Data = "))
+print("Data =",data_int, type(data_int))
+
+# jika ingin input boolean
+data_bool = bool(int(input("Masukan Nilai Boolean = ")))
+print("Data =",data_bool, type(data_bool))
---
+++ 06 - Operasi Aritmatika/Main.py
@@ -0,0 +1,39 @@
+# operasi aritmatika
+a = 10
+b = 3
+
+# operasi tambah +
+hasil = a + b
+print(a, '+',b,'=', hasil)
+
+# operasi pengurangan
+hasil = a - b
+print(a,'-',b,'=', hasil)
+
+# operasi perkalian *
+hasil = a * b
+print(a,'*',b,'=', hasil)
+
+# operasi pembagian /
+hasil = a /b
+print(a,'/',b,'=', hasil)
+      
+# operasi eksponen (pangkat) **
+hasil = a ** b
+print(a, '**',b,'=', hasil)
+
+# operasi modulus %
+hasil = a % b
+print(a, '%',b,'=', hasil)
+
+# operasi floor division //
+hasil = a // b
+print(a,'//',b,'=', hasil)
+
+# prioritas operasi
+"""
+1. ()
+2. Eksponen
+3. Perkalian/Pembagian/Modulus/Floor Division
+4. Penjumlahan/Pengurangan
+"""
